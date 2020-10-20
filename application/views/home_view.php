e5b8f629b3e3c32cee50ae4825536552287bb27a
PauloBaronista <paulo@rpm.com.br>
2020-10-20 11:41:15 -0300

    Ajuste cookies

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -297,18 +297,18 @@
                 </figure>
             </div>
         </div>
-        <!-- <div class="col-xs-12 col-sm-12 col-md-12">
+        <div class="col-xs-12 col-sm-12 col-md-12">
             <div id="cookies" class="col-xs-12 col-sm-12 col-md-12 col-lg-8 col-lg-offset-2 cookies">
                 <div class="col-xs-12 col-sm-9 col-md-9">
                     <p>
-                        Usamos cookies para garantir que obtém sempre a melhor experiência ao visitar o nosso website, para medir a utilização e o desempenho, bem como apresentar anúncios. Ao clicar em “Aceito”, concorda com as nossas configurações padrão.
+                        Usamos cookies para garantir que obtenha sempre a melhor experiência ao visitar o nosso website, para medir a utilização e o desempenho, bem como apresentar anúncios. Ao clicar em “Aceito”, concorda com as nossas configurações padrão.
                     </p>
                 </div>
                 <div class="col-xs-12 col-sm-3 col-md-3">
                     <a href="#" class="btn_cookie btn center-block">Aceito</a>
                 </div>
             </div>
-        </div> -->
+        </div>
         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-8 col-lg-offset-2 logo-footer">
             <img class="img-responsive center-block" src="<?= base_url(); ?>assets/images/logos.png" alt="Logos">
         </div>
